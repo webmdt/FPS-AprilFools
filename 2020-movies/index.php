b1aa126822ce795b687d0be056c82e524443ff51
Marco Taddei <marco@webmdt.com>
2020-03-30 23:00:00 -0400

    2020 - WIP

---
+++ 2020-movies/index.php
@@ -0,0 +1,223 @@
+<?php
+$moviesArray = [
+	["cover"=>"https://img02.mgo-images.com/image/thumbnail?id=MMVF7C152D29230301DF2E49BFB499580B83&amp;ql=70&amp;sizes=310x465", "title"=> "Onward"],
+	["cover"=>"https://img04.mgo-images.com/image/thumbnail?id=MMVC42505A1956D82FEDB67ED902E867DAB7&amp;ql=70&amp;sizes=310x465", "title"=> "The Invisible Man (2020)"],
+	["cover"=>"https://img04.mgo-images.com/image/thumbnail?id=MMV390767535C7B45795FF68C08A8E8F1E18&amp;ql=70&amp;sizes=310x465", "title"=> "The Hunt "],
+	["cover"=>"https://img04.mgo-images.com/image/thumbnail?id=MMVD4CA8ADF6F5DE9C915542F85425AD9F1D&amp;ql=70&amp;sizes=310x465", "title"=> "The Call of the Wild"],
+	["cover"=>"https://img06.mgo-images.com/image/thumbnail?id=MMV3FFCD4C626E61D79BAC131C4E65975E42&amp;ql=70&amp;sizes=310x465", "title"=> "Jumanji: The Next Level"],
+	["cover"=>"https://img09.mgo-images.com/image/thumbnail?id=MMV72016207CBB35D3E06A0CD32776E5DD9C&amp;ql=70&amp;sizes=310x465", "title"=> "Downhill"],
+	["cover"=>"https://img09.mgo-images.com/image/thumbnail?id=MMVD8F270B3467D8FB0C341ABB9AE4490990&amp;ql=70&amp;sizes=310x465", "title"=> "I Still Believe"],
+	["cover"=>"https://img07.mgo-images.com/image/thumbnail?id=MMV7F054EA5D39915A66B73ADC1E2B1AC159&amp;ql=70&amp;sizes=310x465", "title"=> "Bloodshot"],
+	["cover"=>"https://img04.mgo-images.com/image/thumbnail?id=MMVFFE0CD505C20DC7B382E0C729185DD2A8&amp;ql=70&amp;sizes=310x465", "title"=> "Birds of Prey"],
+	["cover"=>"https://img02.mgo-images.com/image/thumbnail?id=MMV6569D04BCE07F1BC19A3BD98E38A488A1&amp;ql=70&amp;sizes=310x465", "title"=> "Vivarium"],
+	["cover"=>"https://img01.mgo-images.com/image/thumbnail?id=MMV2C76ECF85CA1A41C6198C17E53BD03536&amp;ql=70&amp;sizes=310x465", "title"=> "Star Wars: The Rise of Skywalker"],
+	["cover"=>"https://img03.mgo-images.com/image/thumbnail?id=MMV35D4AE84C1D9232D6DF0EE63B456841AA&amp;ql=70&amp;sizes=310x465", "title"=> "Cats"],
+	["cover"=>"https://img04.mgo-images.com/image/thumbnail?id=MMV3B43A8E34C53CE22C569DCE561DA08257&amp;ql=70&amp;sizes=310x465", "title"=> "1917"],
+	["cover"=>"https://img05.mgo-images.com/image/thumbnail?id=MMV40AF377001B738B36874519D0E38182F8&amp;ql=70&amp;sizes=310x465", "title"=> "2099: The Soldier Protocol"],
+	["cover"=>"https://img04.mgo-images.com/image/thumbnail?id=MMVC9E9143F8C05AED7C5FCBC3417D483A1D&amp;ql=70&amp;sizes=310x465", "title"=> "Spies in Disguise"],
+	["cover"=>"https://img06.mgo-images.com/image/thumbnail?id=MMVA8D3E49411378E1F3DF88B03E336B3A4F&amp;ql=70&amp;sizes=310x465", "title"=> "Hunter's Moon"],
+	["cover"=>"https://img08.mgo-images.com/image/thumbnail?id=MMVE8A87005AB8E208AC45909B1F89E1CA15&amp;ql=70&amp;sizes=310x465", "title"=> "Banana Split"],
+	["cover"=>"https://img01.mgo-images.com/image/thumbnail?id=MMV7F96023395277A3762AEB4EA9B4B866BC&amp;ql=70&amp;sizes=310x465", "title"=> "Dolittle"],
+	["cover"=>"https://img09.mgo-images.com/image/thumbnail?id=MMVE3C69DB36038460D20761A461E1F62E83&amp;ql=70&amp;sizes=310x465", "title"=> "The Way Back"],
+	["cover"=>"https://img03.mgo-images.com/image/thumbnail?id=MMV1DC848BBEADCADD650AF96617FFE433BC&amp;ql=70&amp;sizes=310x465", "title"=> "Just Mercy"],
+	["cover"=>"https://img03.mgo-images.com/image/thumbnail?id=MMVD2932726C5CDEC7AC277D3F82BBAEB550&amp;ql=70&amp;sizes=310x465", "title"=> "Emma. (2020)"],
+	["cover"=>"https://img09.mgo-images.com/image/thumbnail?id=MMV2E4F63D2F3C343356393248C80587CD18&amp;ql=70&amp;sizes=310x465", "title"=> "Knives Out"],
+	["cover"=>"https://img02.mgo-images.com/image/thumbnail?id=MMVF3C6F7B2D924CE182A5D94FB51C9853AC&amp;ql=70&amp;sizes=310x465", "title"=> "Uncut Gems"],
+	["cover"=>"https://img01.mgo-images.com/image/thumbnail?id=MMV4EE8E53FFB7F5A9A210C0A8514A9ACD52&amp;ql=70&amp;sizes=310x465", "title"=> "The Gentlemen"],
+	["cover"=>"https://img06.mgo-images.com/image/thumbnail?id=MMVC295347175B0B02534FDD1089B0309B7A&amp;ql=70&amp;sizes=310x465", "title"=> "Ford v Ferrari"],
+	["cover"=>"https://img03.mgo-images.com/image/thumbnail?id=MMVD95D98D3CED75EEF1253578146FBAAD18&amp;ql=70&amp;sizes=310x465", "title"=> "A Beautiful Day in the Neighborhood"],
+	["cover"=>"https://img05.mgo-images.com/image/thumbnail?id=MMVE62354D7EDE037A4E39525D01D21942F7&amp;ql=70&amp;sizes=310x465", "title"=> "Bombshell"],
+	["cover"=>"https://img06.mgo-images.com/image/thumbnail?id=MMV09935488D7D0995ED62E89854CA71EE08&amp;ql=70&amp;sizes=310x465", "title"=> "Richard Jewell "],
+	["cover"=>"https://img09.mgo-images.com/image/thumbnail?id=MMV462B46003DBCE5ADBF236317B8AB5B6CA&amp;ql=70&amp;sizes=310x465", "title"=> "Little Women (2019)"],
+	["cover"=>"https://img09.mgo-images.com/image/thumbnail?id=MMV910D270AFAC15C2939DB09766B373FC02&amp;ql=70&amp;sizes=310x465", "title"=> "Manhunt"],
+	["cover"=>"https://img03.mgo-images.com/image/thumbnail?id=MMVA4419B5F16F23AF19DB052BE844E31B17&amp;ql=70&amp;sizes=310x465", "title"=> "Humboldt: Epic Explorer"],
+	["cover"=>"https://img02.mgo-images.com/image/thumbnail?id=MMVF5FAD104711D8FAD9B8E0F9541DB7BB85&amp;ql=70&amp;sizes=310x465", "title"=> "Frequent Visitors"],
+	["cover"=>"https://img04.mgo-images.com/image/thumbnail?id=MMV69B38D0AE36596829D78EE7AB6E071C73&amp;ql=70&amp;sizes=310x465", "title"=> "Magic"],
+	["cover"=>"https://img03.mgo-images.com/image/thumbnail?id=MMVB15FEE680BAE35F0A9252661681B575E2&amp;ql=70&amp;sizes=310x465", "title"=> "Clemency"],
+	["cover"=>"https://img09.mgo-images.com/image/thumbnail?id=MMV1982DFA60870503C32FA698AE2BF2BC68&amp;ql=70&amp;sizes=310x465", "title"=> "Dark Waters"],
+	["cover"=>"https://img03.mgo-images.com/image/thumbnail?id=MMV6644DEF1E706ACD501AAA7892F3E653CE&amp;ql=70&amp;sizes=310x465", "title"=> "Electric Kingdom"],
+	["cover"=>"https://img07.mgo-images.com/image/thumbnail?id=MMV37B27E6794CEA792D17797FF873BE1A08&amp;ql=70&amp;sizes=310x465", "title"=> "Torn Apart: Separated at the Border"],
+	["cover"=>"https://img05.mgo-images.com/image/thumbnail?id=MMV93F067837937F51A1573E4571F80F0A07&amp;ql=70&amp;sizes=310x465", "title"=> "Midway"],
+	["cover"=>"https://img05.mgo-images.com/image/thumbnail?id=MMV5DE66B54D91DC973CBDC6302B8424A198&amp;ql=70&amp;sizes=310x465", "title"=> "21 Bridges"],
+	["cover"=>"https://img08.mgo-images.com/image/thumbnail?id=MMV7A4D727AC2B67C80E28AAC44F30CCDB26&amp;ql=70&amp;sizes=310x465", "title"=> "Cabal"],
+	["cover"=>"https://img09.mgo-images.com/image/thumbnail?id=MMV5B0E3CCA98CCDA65DB6B6EE475F7CA67E&amp;ql=70&amp;sizes=310x465", "title"=> "Frozen 2"],
+	["cover"=>"https://img03.mgo-images.com/image/thumbnail?id=MMV887DF90AE332D6A6CD2F6B41285836160&amp;ql=70&amp;sizes=310x465", "title"=> "Cunningham"],
+	["cover"=>"https://img05.mgo-images.com/image/thumbnail?id=MMVDE13EA433EC27EC389BC6BCC102637C67&amp;ql=70&amp;sizes=310x465", "title"=> "Joker"],
+	["cover"=>"https://img05.mgo-images.com/image/thumbnail?id=MMV4B9288682DC3A7B8F4CE0F9859AABBC7B&amp;ql=70&amp;sizes=310x465", "title"=> "Jojo Rabbit "],	
+];
+function getMovies($moviesArray){
+	$movies = [];
+	$ratings = ['R','PG13','PG','G','NC17'];
+	$links = [
+		'https://www.youtube.com/watch?v=XCPj4JPbKtA',
+	];
+	
+	foreach($moviesArray as $movie){
+		
+		$randomDate = mt_rand(1585602155,1591747200);
+		$randomRating = array_rand($ratings,1);
+		$randomLink = array_rand($links,1);
+		
+		$movies[] = [
+			'title'   => $movie['title'],
+			'cover'   => $movie['cover'],
+			'link'    => $links[$randomLink],
+			'expireDateTime' => date("D d M - H:i",$randomDate),
+			'length'  => rand(72,145) . ' mins',
+			'rating'  => $ratings[$randomRating],
+		];
+	}
+
+	return $movies;	
+}
+$movies = getMovies($moviesArray);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Movies in Theater</title>
+	<meta name="apple-mobile-web-app-title" value="Movies in Theater">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	<meta name="mobile-web-app-capable" content="yes"> 
+	<link rel="apple-touch-startup-image" href="http://cdn.fairfieldschools.org/district-v2/assets/img/app-icon/fps-icon-192px%402x-precomposed.png">
+	<link rel="shortcut" href="http://cdn.fairfieldschools.org/district-v2/assets/img/app-icon/fps-icon-192px%402x-precomposed.png" />
+	<link rel="icon" sizes="192x192" href="http://cdn.fairfieldschools.org/district-v2/assets/img/app-icon/fps-icon-192px%402x-precomposed.png">
+	<link rel="shortcut icon" href="http://cdn.fairfieldschools.org/district-v2/assets/img/app-icon/fps-icon-192px%402x-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" href="http://cdn.fairfieldschools.org/district-v2/assets/img/app-icon/fps-icon-192px%402x-precomposed.png">
+	<link rel="apple-touch-icon-precomposed apple-touch-icon" href="http://cdn.fairfieldschools.org/district-v2/assets/img/app-icon/fps-icon-192px%402x-precomposed.png"> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="./assets/main.css?v1">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/solid.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/fontawesome.min.js"></script>
+	<script>
+		//https://codepen.io/JacobLett/pen/xqpEYE
+		$(document).ready(function() {
+			
+			// Gets the video src from the data-src on each button
+			var $videoSrc;  
+			$('.video-btn').click(function() {
+			    $videoSrc = $(this).data( "src" );
+			});
+			console.log($videoSrc);
+		  
+			// when the modal is opened autoplay it  
+			$('#myModal').on('shown.bs.modal', function (e) {
+				    
+				// set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+				$("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0" ); 
+			})
+			  
+			// stop playing the youtube video when I close the modal
+			$('#myModal').on('hide.bs.modal', function (e) {
+				// a poor man's stop video
+				$("#video").attr('src',$videoSrc); 
+			}) 
+		});
+	</script>
+  </head>
+  <body>
+  	<div class="container">
+	  	<div class="row no-gutters">
+			<div class="col-8 no-gutters"><img src="assets/homePremiere.jpg" alt="homePremiere" class="img-fluid"></div>
+			<div class="col-4 no-gutters"><img src="assets/moreMovies.jpg" alt="moreMovies" class="img-fluid"></div>
+	  	</div>
+  	</div>
+  	<div class="container">
+  	<?php
+	foreach($movies as $movie){
+	?>
+	
+		<div class="movie-card">
+			<div class="movie-header" style="background: url(<?php echo $movie["cover"];?>); background-size: cover;">
+				<div class="header-icon-container text-center">
+					<a class="video-btn" data-toggle="modal" href="<?php echo $movie["link"];?>" data-target="#myModal">
+						<i class="fas fa-play header-icon"></i>
+					</a>
+				</div>
+			</div><!--movie-header-->
+			<div class="movie-content">
+				<div class="movie-content-header">
+					<a href="<?php echo $movie["link"];?>">
+						<h3 class="movie-title"><?php echo $movie["title"];?></h3>
+					</a>
+				</div>
+				<div class="movie-info">
+					<div class="info-section">
+						<label>Expires On</label>
+						<span><?php echo $movie["expireDateTime"];?></span>
+					</div><!--date,time-->
+					<div class="info-section">
+						<label>Movie Length</label>
+						<span><?php echo $movie["length"];?></span>
+					</div><!--screen-->
+					<div class="info-section">
+						<label>Rating</label>
+						<span><?php echo $movie["rating"];?></span>
+					</div><!--row-->
+				</div>
+			</div><!--movie-content-->
+		</div><!--movie-card-->  	
+	  	
+	<?php  	
+  	}
+  	?>
+	
+		<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-body">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>        
+						<div class="embed-responsive embed-responsive-16by9">
+							<iframe class="embed-responsive-item" src="" id="video"  allowscriptaccess="always" allow="autoplay"></iframe>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>  
+
+	</div><!--container-->
+<!--
+<div class="movie-card">
+	<div class="movie-header" style="background: url('http://henrycavill.org/images/Films/2013-Man-of-Steel/posters/3-Walmart-Superman-a.jpg'); background-size: cover;">
+		<div class="header-icon-container text-center">
+			<a href="#">
+				<i class="fas fa-play header-icon"></i>
+			</a>
+		</div>
+	</div>
+	<div class="movie-content">
+		<div class="movie-content-header">
+			<a href="#">
+				<h3 class="movie-title">Man of Steel</h3>
+			</a>
+			<div class="fps-logo"></div>
+		</div>
+		<div class="movie-info">
+			<div class="info-section">
+				<label>Expires On</label>
+				<span>Sun 8 Sept - 10:00PM</span>
+			</div>
+			<div class="info-section">
+				<label>Movie Length</label>
+				<span>92 min</span>
+			</div>
+			<div class="info-section">
+				<label>Rating</label>
+				<span>R</span>
+			</div>
+		</div>
+	</div>
+</div>		
+-->	
+  </body>
+</html>
